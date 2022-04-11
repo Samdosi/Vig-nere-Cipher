f4fd6cd9c69f092711e6a165e51002fbe7270c99
Mohammad Ali Dosi <87053736+Samdosi@users.noreply.github.com>
2022-04-10 22:16:29 -0400

    Update pa01.java

--- pa01.java
+++ pa01.java
@@ -1,26 +1,3 @@
-/*=============================================================================
-| Assignment: pa01 - Encrypting a plaintext file using the Vigenere cipher
-|
-| Author: Mohammad Ali Dosi
-| Language: Java
-|
-| To Compile: javac pa01.java
-| gcc -o pa01 pa01.c
-| g++ -o pa01 pa01.cpp
-|
-| To Execute: java -> java pa01 kX.txt pX.txt
-| or c++ -> ./pa01 kX.txt pX.txt
-| or c -> ./pa01 kX.txt pX.txt
-| where kX.txt is the keytext file
-| and pX.txt is plaintext file
-|
-| Note: All input files are simple 8 bit ASCII input
-|
-| Class: CIS3360 - Security in Computing - Spring 2022
-| Instructor: McAlpin
-| Due Date: per assignment
-|
-+=============================================================================*/
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Locale;
@@ -159,11 +136,3 @@ public class pa01
         scan.close();
     }
 }
-/*=============================================================================
-| I [Mohammad Ali Dosi] ([mo346779]) affirm that this program is
-| entirely my own work and that I have neither developed my code together with
-| any another person, nor copied any code from any other person, nor permitted
-| my code to be copied or otherwise used by any other person, nor have I
-| copied, modified, or otherwise used programs created by others. I acknowledge
-| that any violation of the above terms will be treated as academic dishonesty.
-+=============================================================================*/
